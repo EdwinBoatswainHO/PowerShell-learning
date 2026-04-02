ed80155f85248f56b5806cd8ed7820c8b6fb814d
EdwinBoatswainHO <edwin.boatswain@homeoffice.gov.uk>
2026-04-02 12:21:31 +0100

    Added implementation and basic validation

--- codewars/CreatePhoneNumber/New-PhoneNumber.Tests.ps1
+++ codewars/CreatePhoneNumber/New-PhoneNumber.Tests.ps1
@@ -1,4 +1,4 @@
-# These are the CodeWars tests
+# First 2 tests are code wars. Remaining tests exercise validation
 BeforeAll {
     . "$PSScriptRoot/New-PhoneNumber.ps1"
 }
@@ -13,4 +13,23 @@ Describe "Fixed Tests" {
 		[int[]]$numbers = @(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 		New-PhoneNumber($numbers) | Should -Be "(111) 111-1111"
 	}
+
+    it "Should handle arrays too big" {
+        [int[]]$numbers = @(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+		New-PhoneNumber($numbers) | Should -Be ""
+    }
+    
+    it "Should handle arrays too small" {
+        [int[]]$numbers = @(1, 1)
+        New-PhoneNumber($numbers) | Should -Be ""
+    }
+
+    it "Should handle arrays too big" {
+        [int[]]$numbers = @(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+		New-PhoneNumber($numbers) | Should -Be ""
+    }
+    it "Should reject arrays containing numbers outside of range [0-9]" {
+        [int[]]$numbers = @(1, 1, 1, 1, 1, 1, 1, 1, 1, 10)
+		New-PhoneNumber($numbers) | Should -Be ""
+    }
 }
--- codewars/CreatePhoneNumber/New-PhoneNumber.ps1
+++ codewars/CreatePhoneNumber/New-PhoneNumber.ps1
@@ -1,4 +1,19 @@
 function New-PhoneNumber([int[]]$numbers)
 {
-	#TODO code solution
+    $sz = $numbers.Length
+    if ($sz -ne 10) {
+        return ""
+    }
+    # What to do with numbers out of range?
+    foreach ($number in $numbers) {
+        if (($number -lt 0) -or ($number -gt 9)) {
+            return ""
+        }
+    }
+
+    # return string in the correct format after validation
+
+    return "($($numbers[0])$($numbers[1])$($numbers[2])) " +
+           "$($numbers[3])$($numbers[4])$($numbers[5])-" +
+           "$($numbers[6])$($numbers[7])$($numbers[8])$($numbers[9])"
 }
